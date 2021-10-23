fd2583909c093417ad58a095a718a4f19f727ecd
Your Name <you@example.com>
2021-10-23 13:48:31 -0800

    finally done!

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ class App extends Component {
         }).then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('https://mighty-bayou-95770.herokuapp.com:3000/image', {
+        fetch('https://mighty-bayou-95770.herokuapp.com/image', {
        method: 'put',
        headers: {'Content-Type': 'application/json'},
        body: JSON.stringify({
